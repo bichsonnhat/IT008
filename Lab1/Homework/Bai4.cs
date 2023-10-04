a8e303e08134b6755c027663161c9192a0427116
vhq3404 <22521195@gm.uit.edu.vn>
2023-10-04 22:19:45 +0700

    Create Bai4.cs

---
+++ Lab1/Homework/Bai4.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+public class NhietKe
+{
+    public event EventHandler<double> TemperatureChanged;
+    private Random random = new Random();
+    private double temperature;
+
+    public double Temperature
+    {
+        get { return temperature; }
+        private set
+        {
+            temperature = value;
+            OnTemperatureChanged(temperature);
+        }
+    }
+
+    public void RandomTemperature()
+    {
+        Temperature = 35 + random.NextDouble() * (42 - 35);
+    }
+
+    protected virtual void OnTemperatureChanged(double newTemperature)
+    {
+        TemperatureChanged?.Invoke(this, newTemperature);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        NhietKe nhietke = new NhietKe();
+
+        nhietke.TemperatureChanged += (sender, temperature) =>
+        {
+            Console.OutputEncoding = UnicodeEncoding.Unicode;
+            Console.InputEncoding = UnicodeEncoding.Unicode;
+            Console.WriteLine($"Nhiệt độ hiện tại: {temperature:F2}°C");
+        };
+
+        while (true)
+        {
+            nhietke.RandomTemperature();
+            Thread.Sleep(1000);
+        }
+    }
+}
