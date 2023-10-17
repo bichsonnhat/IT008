fbe7e8d4d7bd8a9e9cee7a87708fb68470464b3c
AKaLe17 <62697217+AKaLee-IK27@users.noreply.github.com>
2023-10-17 23:05:12 +0700

    Bài 6

---
+++ Lab1/Homework/Bai6.cs
@@ -0,0 +1,39 @@
+﻿namespace Bai6
+{
+    class Mamal
+    {
+        public string? characteristics;
+    }    
+
+    class Whale : Mamal
+    {
+        Whale() { }
+    }
+
+    interface IThinking
+    {
+        public void thinking_behavior();
+    }
+
+    interface IIntelligent
+    {
+        public void intelligent_behavior();
+    }
+
+    interface IAbility : IThinking, IIntelligent {}
+
+    class Human : Mamal, IAbility
+    {
+        Human() { }
+        public void intelligent_behavior() { };
+        public void thinking_behavior() { }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
